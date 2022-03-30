8c12a39bfb9d641f41e72760af5259a9efa7fb12
Berestov <berestovvanya@gmail.com>
2022-03-30 21:21:41 +0300

    Created entities for database.

--- Forum/Startup.cs
+++ Forum/Startup.cs
@@ -1,13 +1,12 @@
+using DAL.DbContext;
+using DAL.Entity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Forum
 {
@@ -20,10 +19,19 @@ namespace Forum
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().AddSessionStateTempDataProvider();
+            services.AddSession();
+
             services.AddControllersWithViews();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.
+          UseSqlServer(Configuration.GetConnectionString("Forum")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+       .AddEntityFrameworkStores<ApplicationDbContext>()
+       .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +44,6 @@ namespace Forum
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
